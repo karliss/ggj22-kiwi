1590ec0b18d75bd1468bdbebf84156b8b8b35ebf
Kārlis Seņko <karlis3p70l1ij@gmail.com>
2022-01-29 14:34:05 +0200

    Add text writing mode in the editor.

--- src/game.rs
+++ src/game.rs
@@ -9,6 +9,7 @@ use crossterm::{
     style::{self, Color, Attribute, Stylize},
     terminal::{self, disable_raw_mode, enable_raw_mode},
 };
+use crossterm::terminal::{Clear, ClearType};
 
 use level::Level;
 use ui::UiWidget;
@@ -24,7 +25,9 @@ pub struct LevelEditor
     level: Level,
     cursor_pos: V2,
     view_corner: V2,
+    wrap_pos: V2,
     need_refresh: bool,
+    mode: EditorMode,
 }
 
 fn buffer_size() -> (u16, u16)
@@ -42,6 +45,12 @@ fn get_color(c: CellColor) -> Color {
     }
 }
 
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+enum EditorMode {
+    View,
+    WriteText,
+}
+
 impl LevelEditor {
     pub fn new(ui: &mut UiContext) -> LevelEditor {
         let mut result = LevelEditor {
@@ -49,7 +58,9 @@ impl LevelEditor {
             level: Level::new(50, 50),
             cursor_pos: V2::new(),
             view_corner: V2::new(),
+            wrap_pos: V2::new(),
             need_refresh: true,
+            mode: EditorMode::View,
         };
         result.fill_level();
         result
@@ -64,9 +75,9 @@ impl LevelEditor {
                 if x % 2 == 1 {
                     cell.background = CellColor::White;
                     cell.foreGround = CellColor::Black;
-                }
-                if x % 2 == y % 2 {
-                    cell.letter = '#';
+                } else {
+                    cell.background = CellColor::Black;
+                    cell.foreGround = CellColor::White;
                 }
                 self.level.set(pos, cell);
             }
@@ -74,12 +85,26 @@ impl LevelEditor {
     }
 
 
-    fn print_level(&mut self, ui: &mut UiContext) -> std::io::Result<()> {
-        let size = ui.buffer_size();
-        let mut visible_rect = vecmath::Rectangle {
+    fn get_view_rect(&self) -> Rectangle {
+        let size = buffer_size();
+        vecmath::Rectangle {
             pos: self.view_corner,
             size: V2::from(size),
-        };
+        }
+    }
+
+    fn print_status_bar(&mut self, ui: &mut UiContext) -> std::io::Result<()> {
+        let size = ui.buffer_size();
+        queue!(ui.stdout, cursor::MoveTo(0, size.1 - 1),
+                style::ResetColor)?;
+        queue!(ui.stdout, style::Print(format!("mode: {:?}", self.mode)))?;
+        queue!(ui.stdout, Clear(ClearType::UntilNewLine))?;
+        Ok(())
+    }
+
+    fn print_level(&mut self, ui: &mut UiContext) -> std::io::Result<()> {
+        let size = ui.buffer_size();
+        let mut visible_rect = self.get_view_rect();
         queue!(ui.stdout, cursor::Hide)?;
         for y in 0..size.1 {
             let mut reposition = true;
@@ -100,6 +125,8 @@ impl LevelEditor {
                         .on(get_color(cellData.background))))?;
             }
         }
+        self.print_status_bar(ui)?;
+
         if visible_rect.contains(self.cursor_pos) {
             let cpos = self.cursor_pos - self.view_corner;
             queue!(ui.stdout, cursor::MoveTo(cpos.x as u16, cpos.y as u16),
@@ -108,6 +135,29 @@ impl LevelEditor {
             queue!(ui.stdout, cursor::Hide)?
         }
         Ok(())
+    }
+
+    fn keep_cursor_in_view(&mut self) {
+        let PADDING = 2;
+        let mut view = self.get_view_rect();
+        view = view.grow(-PADDING);
+        if view.contains(self.cursor_pos) {
+            return;
+        }
+        let size = V2::from(buffer_size());
+        let pos = self.cursor_pos;
+        if pos.x < view.left() {
+            self.view_corner.x = pos.x - PADDING;
+        }
+        if pos.x > view.right() {
+            self.view_corner.x = pos.x + PADDING - size.x;
+        }
+        if pos.y < view.top() {
+            self.view_corner.y = pos.y - PADDING;
+        }
+        if pos.y > view.bottom() {
+            self.view_corner.y = pos.y + PADDING - size.y;
+        }
     }
 }
 
@@ -123,42 +173,93 @@ impl UiWidget for LevelEditor {
 
     fn input(&mut self, e: &Event) -> Option<UiEvent> {
         self.mark_refresh(true);
-        match e {
+        let v = match e {
             Event::Key(KeyEvent { code: KeyCode::Up, modifiers: KeyModifiers::NONE }) => {
                 self.cursor_pos = self.cursor_pos + V2::make(0, -1);
+                self.keep_cursor_in_view();
                 self.event(UiEventType::Changed)
             }
             Event::Key(KeyEvent { code: KeyCode::Down, modifiers: KeyModifiers::NONE }) => {
                 self.cursor_pos = self.cursor_pos + V2::make(0, 1);
+                self.keep_cursor_in_view();
                 self.event(UiEventType::Changed)
             }
             Event::Key(KeyEvent { code: KeyCode::Left, modifiers: KeyModifiers::NONE }) => {
                 self.cursor_pos = self.cursor_pos + V2::make(-1, 0);
+                self.keep_cursor_in_view();
                 self.event(UiEventType::Changed)
             }
             Event::Key(KeyEvent { code: KeyCode::Right, modifiers: KeyModifiers::NONE }) => {
                 self.cursor_pos = self.cursor_pos + V2::make(1, 0);
+                self.keep_cursor_in_view();
                 self.event(UiEventType::Changed)
             }
 
-            Event::Key(KeyEvent { code: KeyCode::Char('w'), modifiers: KeyModifiers::NONE }) => {
-                self.view_corner = self.view_corner + V2::make(0, -1);
+            Event::Key(KeyEvent { code: KeyCode::F(2), modifiers: KeyModifiers::NONE }) => {
+                self.mode = EditorMode::View;
                 self.event(UiEventType::Changed)
             }
-            Event::Key(KeyEvent { code: KeyCode::Char('s'), modifiers: KeyModifiers::NONE }) => {
-                self.view_corner = self.view_corner + V2::make(0, 1);
+            Event::Key(KeyEvent { code: KeyCode::F(3), modifiers: KeyModifiers::NONE }) => {
+                self.mode = EditorMode::WriteText;
+                self.wrap_pos = self.cursor_pos;
                 self.event(UiEventType::Changed)
             }
-            Event::Key(KeyEvent { code: KeyCode::Char('a'), modifiers: KeyModifiers::NONE }) => {
-                self.view_corner = self.view_corner + V2::make(-1, 0);
-                self.event(UiEventType::Changed)
-            }
-            Event::Key(KeyEvent { code: KeyCode::Char('d'), modifiers: KeyModifiers::NONE }) => {
-                self.view_corner = self.view_corner + V2::make(1, 0);
+            Event::Key(KeyEvent { code: KeyCode::F(4), modifiers: KeyModifiers::NONE }) => {
+                self.wrap_pos = self.cursor_pos;
                 self.event(UiEventType::Changed)
             }
             _ => None
+        };
+        if v.is_some() { return v; }
+
+        if self.mode != EditorMode::WriteText {
+            let v = match e {
+                Event::Key(KeyEvent { code: KeyCode::Char('w'), modifiers: KeyModifiers::NONE }) => {
+                    self.view_corner = self.view_corner + V2::make(0, -1);
+                    self.event(UiEventType::Changed)
+                }
+                Event::Key(KeyEvent { code: KeyCode::Char('s'), modifiers: KeyModifiers::NONE }) => {
+                    self.view_corner = self.view_corner + V2::make(0, 1);
+                    self.event(UiEventType::Changed)
+                }
+                Event::Key(KeyEvent { code: KeyCode::Char('a'), modifiers: KeyModifiers::NONE }) => {
+                    self.view_corner = self.view_corner + V2::make(-1, 0);
+                    self.event(UiEventType::Changed)
+                }
+                Event::Key(KeyEvent { code: KeyCode::Char('d'), modifiers: KeyModifiers::NONE }) => {
+                    self.view_corner = self.view_corner + V2::make(1, 0);
+                    self.event(UiEventType::Changed)
+                }
+                _ => None
+            };
+            if v.is_some() {
+                return v;
+            }
         }
+
+        if self.mode == EditorMode::WriteText {
+            let v = match e {
+                Event::Key(KeyEvent { code: KeyCode::Enter, modifiers: KeyModifiers::NONE }) => {
+                    self.cursor_pos.x = self.wrap_pos.x;
+                    self.cursor_pos.y += 1;
+                    self.event(UiEventType::Changed)
+                }
+                Event::Key(KeyEvent { code: KeyCode::Esc, modifiers: KeyModifiers::NONE }) => {
+                    self.mode = EditorMode::View;
+                    self.event(UiEventType::Changed)
+                }
+                Event::Key(KeyEvent { code: KeyCode::Char(c), modifiers: KeyModifiers::NONE }) if !c.is_control() => {
+                    let mut data = self.level[self.cursor_pos];
+                    data.letter = *c;
+                    self.level.set(self.cursor_pos, data);
+                    self.cursor_pos.x += 1;
+                    self.event(UiEventType::Changed)
+                }
+                _ => None
+            };
+        }
+
+        None
     }
 
     fn child_widgets(&self) -> Vec<&dyn UiWidget> {
